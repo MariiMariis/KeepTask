a72ee4e65ff4ea05f01fc139ee4e5e11e4a5a360
MariiMariis <mariana.motta94@gmail.com>
2022-03-21 18:47:01 -0300

    Navegação Inicial

---
+++ app/src/main/java/br/infnet/dev/keeptask/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.infnet.dev.keeptask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import br.infnet.dev.keeptask.R
+import br.infnet.dev.keeptask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
+    }
+}
