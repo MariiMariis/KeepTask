020fc9354f2e9d0518709e822869adfea8ea05c1
MariiMariis <mariana.motta94@gmail.com>
2022-03-21 21:41:59 -0300

    Adicionando funcionalidade de recuperação de conta e criação de usuário com sync no Firebase

--- app/src/main/java/br/infnet/dev/keeptask/ui/auth/LoginFragment.kt
+++ app/src/main/java/br/infnet/dev/keeptask/ui/auth/LoginFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import br.infnet.dev.keeptask.R
 import br.infnet.dev.keeptask.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +32,16 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         initClicks()
     }
 
     private fun initClicks() {
+        binding.btnLogin.setOnClickListener {
+            validateData()
+        }
+
         binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.registerFragment)
         }
@@ -38,6 +51,40 @@ class LoginFragment : Fragment() {
         }
     }
 
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if(email.isNotEmpty()){
+            if(password.isNotEmpty()){
+
+                binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
+            } else{
+                Toast.makeText(requireContext(), "Por favor, insira uma senha", Toast.LENGTH_SHORT).show()
+
+            }
+        } else{
+            Toast.makeText(requireContext(), "Por favor, informe seu email", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                }
+            }
+
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
