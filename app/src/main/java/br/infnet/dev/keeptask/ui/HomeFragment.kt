1c7c93922845f0dc622582c5b62315ff5b42eebe
MariiMariis <mariana.motta94@gmail.com>
2022-03-21 22:32:13 -0300

    Adicionando fragments para adição de tarefas.

--- app/src/main/java/br/infnet/dev/keeptask/ui/HomeFragment.kt
+++ app/src/main/java/br/infnet/dev/keeptask/ui/HomeFragment.kt
@@ -6,15 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.infnet.dev.keeptask.R
+import br.infnet.dev.keeptask.databinding.FragmentHomeBinding
+import br.infnet.dev.keeptask.databinding.FragmentLoginBinding
+import br.infnet.dev.keeptask.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configTabLayout()
+    }
+
+
+    private fun configTabLayout() {
+        val adapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = adapter
+
+        adapter.addFragment(TodoFragment(), "A iniciar")
+        adapter.addFragment(DoingFragment(), "Em progresso")
+        adapter.addFragment(DoneFragment(), "Finalizado")
+
+        binding.viewPager.offscreenPageLimit = adapter.itemCount
+
+        TabLayoutMediator(
+            binding.tabs, binding.viewPager
+        ) {
+            tab, position ->
+            tab.text = adapter.getTitle(
+                position
+            )
+        }.attach()
     }
 
 }
