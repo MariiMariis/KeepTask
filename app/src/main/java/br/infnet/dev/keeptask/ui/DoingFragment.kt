97514ab411e745e2466fd06a0aab3647646b7f18
MariiMariis <mariana.motta94@gmail.com>
2022-03-30 15:09:50 -0300

    Adding remove option for itens

--- app/src/main/java/br/infnet/dev/keeptask/ui/DoingFragment.kt
+++ app/src/main/java/br/infnet/dev/keeptask/ui/DoingFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.infnet.dev.keeptask.R
 import br.infnet.dev.keeptask.databinding.FragmentDoingBinding
+import br.infnet.dev.keeptask.helper.FirebaseHelper
+import br.infnet.dev.keeptask.model.Task
+import br.infnet.dev.keeptask.ui.adapter.TaskAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class DoingFragment : Fragment() {
 
     private var _binding: FragmentDoingBinding? =null
     private val binding get() = _binding!!
+
+    private lateinit var taskAdapter: TaskAdapter
+    private val taskList = mutableListOf<Task>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +33,85 @@ class DoingFragment : Fragment() {
         _binding = FragmentDoingBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClicks()
+        getTask()
+    }
+
+    private fun initClicks() {
+        binding.fabAdd.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+
+        }
+    }
+
+    private fun getTask() {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()) {
+
+                        taskList.clear()
+
+                        for (snap in snapshot.children) {
+                            val task = snap.getValue(Task::class.java) as Task
+
+                            if(task.status == 1) taskList.add(task)
+                        }
+
+                        binding.textInfo.text = ""
+                        taskList.reverse()
+                        initAdapter()
+
+                    }else{
+                        binding.textInfo.text = "Nenhuma tarefa cadastrada"
+
+                    }
+                    binding.progressBar.isVisible = false
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(requireContext(), "Erro", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }
+
+    private fun initAdapter() {
+        binding.rvTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTask.setHasFixedSize(true)
+        taskAdapter = TaskAdapter(requireContext(), taskList) { task, select ->
+            optionSelected(task, select)
+        }
+        binding.rvTask.adapter = taskAdapter
+    }
+
+    private fun optionSelected(task:Task, select: Int) {
+        when(select) {
+            TaskAdapter.SELECT_REMOVE -> {
+                deleteTask(task)
+            }
+        }
+    }
+
+    private fun deleteTask(task:Task) {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .child(task.id)
+            .removeValue()
+
+        taskList.remove(task)
+        taskAdapter.notifyDataSetChanged()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/br/infnet/dev/keeptask/ui/TodoFragment.kt
+++ app/src/main/java/br/infnet/dev/keeptask/ui/TodoFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.infnet.dev.keeptask.R
 import br.infnet.dev.keeptask.databinding.FragmentTodoBinding
+import br.infnet.dev.keeptask.helper.FirebaseHelper
+import br.infnet.dev.keeptask.model.Task
+import br.infnet.dev.keeptask.ui.adapter.TaskAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 
 class TodoFragment : Fragment() {
 
     private var _binding: FragmentTodoBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var taskAdapter: TaskAdapter
+    private val taskList = mutableListOf<Task>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +39,7 @@ class TodoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initClicks()
+        getTask()
     }
 
     private fun initClicks() {
@@ -34,6 +47,71 @@ class TodoFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
 
         }
+    }
+
+
+    private fun getTask() {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()) {
+
+                        taskList.clear()
+
+                        for (snap in snapshot.children) {
+                            val task = snap.getValue(Task::class.java) as Task
+
+                            if(task.status == 0) taskList.add(task)
+                        }
+
+                        binding.textInfo.text = ""
+                        taskList.reverse()
+                        initAdapter()
+
+                    }else{
+                        binding.textInfo.text = "Nenhuma tarefa cadastrada"
+
+                    }
+                    binding.progressBar.isVisible = false
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(requireContext(), "Erro", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }
+
+    private fun initAdapter() {
+        binding.rvTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTask.setHasFixedSize(true)
+        taskAdapter = TaskAdapter(requireContext(), taskList) { task, select ->
+            optionSelected(task, select)
+        }
+        binding.rvTask.adapter = taskAdapter
+    }
+
+    private fun optionSelected(task:Task, select: Int) {
+        when(select) {
+            TaskAdapter.SELECT_REMOVE -> {
+                deleteTask(task)
+            }
+        }
+    }
+
+    private fun deleteTask(task:Task) {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .child(task.id)
+            .removeValue()
+
+        taskList.remove(task)
+        taskAdapter.notifyDataSetChanged()
     }
 
     override fun onDestroyView() {
